f8fd7f3d42adf8e4933b9e0cc556a6fc3b91c5d6
GOD <masterionic@gmail.com>
2020-03-26 23:28:13 +0800

    exchange section

---
+++ public/javascripts/custom.js
@@ -0,0 +1,15 @@
+$('.dropdown').on('show.bs.dropdown', function (e) {
+    $(this).find('.dropdown-menu').first().stop(true, true).slideDown(300);
+});
+
+$('.dropdown').on('hide.bs.dropdown', function (e) {
+    $(this).find('.dropdown-menu').first().stop(true, true).slideUp(200);
+});
+
+$(".dropdown-menu-coin3 a").on('click', function () {
+    $("#dropdowncoin3 .selected-coin3").text($(this).text());
+});
+
+$(".dropdown-menu-coin2 a").on('click', function () {
+    $("#dropdowncoin2 .selected-coin2").text($(this).text());
+});
